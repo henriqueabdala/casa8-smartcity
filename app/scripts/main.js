4c0fb46decf6daea572738b6024b42169640b541
Henrique Abdala <hmabdala@gmail.com>
2021-12-18 19:24:12 -0300

    insert footer and more

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
 console.log('\'Allo \'Allo!');
+
+new VenoBox({
+    selector: '.venobox-video',
+});
+    // $(".venobox-gallery").venobox({ framewidth: "85vw", frameheight: "85vh" });
+    // $(".venobox-frame").venobox({ framewidth: "90vw", frameheight: "90vh" });
